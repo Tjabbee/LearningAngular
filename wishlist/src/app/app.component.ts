a17f5dcbaf6fb89f429b67e8d0f3327c5ed9bcc2
Charbel Salame <charbel.salame99@gmail.com>
2025-03-14 20:57:37 +0100

    Started to work on the wishlist.

--- wishlist/src/app/app.component.ts
+++ wishlist/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { WishItem } from '../shared/models/wishItem';
 
 @Component({
   selector: 'app-root',
@@ -8,5 +9,10 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+  items = [
+    new WishItem('To Learn Angular'),
+    new WishItem('Get Coffee', true),
+    new WishItem('Find grass that cuts itself')
+  ];
   title = 'wishlist';
 }
